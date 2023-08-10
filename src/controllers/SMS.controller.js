68f2b8f6150cc97cb284572c91f4930022330ca1
Nguyen Nguyen Khang <khangnguyenkn19999@gmail.com>
2023-08-10 10:24:39 +0700

    commit 10/08/2023

--- src/controllers/SMS.controller.js
+++ src/controllers/SMS.controller.js
@@ -1,6 +1,6 @@
 const twilio = require("twilio")
 const accountSid = "AC519fff8de5182c34a2a96e8075865153"
-const authToken = "d1bf8907c75fd411910f60e0786f27b3"
+const authToken = "cefe4e4baae097975fef0b2574a237b2"
 const client = twilio(accountSid, authToken)
 function generateVerificationCode() {
 	return Math.floor(Math.random() * 1000000)
